fa1a309e77e14ac597b3d3a5ef883dfcdd4a8d3f
zeralot <zerozeralot@gmail.com>
2019-03-22 03:21:32 +0700

    Add comment
    Add some comment

--- checkFinal.py
+++ checkFinal.py
@@ -1,3 +1,4 @@
+# Import
 import requests
 import re
 import time
@@ -7,7 +8,7 @@ import getopt
 import ntpath
 from random import randint
 
-
+# Get random User agent for header
 def getRandomUserAgent():
     user_agents = ["Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:38.0) Gecko/20100101 Firefox/38.0",
                    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0",
@@ -25,21 +26,27 @@ def getRandomUserAgent():
                    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0"]
     return user_agents[randint(0, len(user_agents) - 1)]
 
-
+# Main
 def main(argv):
     inputfile = ''
     outputfile = ''
     try:
         opts, args = getopt.getopt(argv, "i:", ["ifile="])
     except getopt.GetoptError:
+        # Print help syntax
         print('checkFinal.py -i <inputfile>')
         sys.exit(2)
     for opt, arg in opts:
+        # Case arg -h
+        # Print help syntax
         if opt == '-h':
             print('checkFinal.py -i <inputfile>')
             sys.exit()
+        # Case arg -i or --ifile
         elif opt in ("-i", "--ifile"):
+            # Get input File
             inputfile = arg
+    # Define output File
     outputfile = "output\output_"+ntpath.basename(inputfile)
     print('-' * 50)
     print('|| Input file is      :', inputfile)
@@ -47,40 +54,52 @@ def main(argv):
     print('-' * 50)
 
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    # Start time count
     start = time.clock()
     print('Scanning' + ' .' * 15)
+    # Open input file in read mode
     with open(inputfile, "r") as ins:
-        array = []
         for line in ins:
+            # Get host as each line of input file
             host = "http://"+line.strip()
+            # Get random user agent and set to header
             headers = {
                 'User-Agent': getRandomUserAgent()
             }
             try:
+                # Request to CHANGELOG.txt of host
                 r = requests.post(host+"/CHANGELOG.txt",
                                   verify=False, headers=headers, timeout=1)
+                # Case status code != 200
                 if(r.status_code != 200):
+                    # Request to CHANGELOG.txt of host
                     r = requests.post(
                         host+"/core/CHANGELOG.txt", verify=False, headers=headers, timeout=1)
+                # Get data
                 data = r.text
             except Exception as e:
                 data = ""
+            # Case check drupal
             if "Drupal 1.0.0, 2001-01-15" in data and "<!doctype html>" not in data and "<!DOCTYPE html>" not in data:
                 check = True
                 sline = 0
                 while check:
                     try:
+                        # Get newest version of drupal
                         data = r.text.split('\n')[sline]
                     except Exception as e:
-                        # print("no content")
                         check = False
                     if "Drupal" in data and "xxxx" not in data and "content=" not in data:
                         check = False
                     else:
                         sline = sline+1
+                # Concate to result
                 result = host+" "+data
+                # Open output file
                 with open(outputfile, 'a') as f:
+                    # Write the result to file
                     f.write("%s\n" % result.encode("utf-8"))
+    # Open output file and write the total time scanning
     with open(outputfile, 'a') as f:
         f.write("------| Total Time: %s |-------\n" % (time.clock() - start))
 
